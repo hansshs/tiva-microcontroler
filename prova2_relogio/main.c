ae132b459c219e2913f44a51d5993acdc9c78879
hansshs <hansherbert99@gmail.com>
2023-11-22 10:52:37 -0300

    alpha version - kinda works

--- prova2_relogio/main.c
+++ prova2_relogio/main.c
@@ -1,6 +1,8 @@
 #include "Nokia5110.h"
 #include "driverlib/ssi.h"
 #include "inc/hw_memmap.h"
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/gpio.h"
@@ -11,13 +13,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
-uint8_t hour = 0, minute = 0, second = 0;
+//************************* CONSTS *****************************
+#define NUM_HORAS 12
+#define NUM_MIN 60
+#define NUM_SEC 60
+uint8_t hour = 12, minute = 59, second = 50; //Armistice WW1 -> "at the eleventh hour of the eleventh day of the eleventh monthï¿½the guns fell silent"
+uint32_t ui32Period;
+bool flipflop = true;
 
+//************************* UART HANDLER *****************************
 //Este handler, recebe os chars e executa rotina para armazenar na forma de tres variaveis hour, minute, second
 void UARTIntHandler(void)
 {
-    static uint8_t currentField = 0;  // 0 - Hour, 1 - Minute, 2 - Second, 3- Reset
+    static uint8_t currentField = 0;  // 0 - Hour, 1 - Minute, 2 - Second
+
 
     uint32_t ui32Status;
     ui32Status = UARTIntStatus(UART0_BASE, true); //get interrupt status
@@ -32,40 +43,43 @@ void UARTIntHandler(void)
 
         if (receivedChar == ':')
         {
-            currentField++;  // Muda para o próximo campo ao encontrar ":"
+            currentField++;  // Muda para o prï¿½ximo campo ao encontrar ":"
+        }
+
+        if (receivedChar == '-') { //Variavel de Reset
+            hour   = 0;
+            minute = 0;
+            second = 0;
+            currentField = 0;
         }
         else
         {
-            // Converte o caractere para um número
+            // Converte o caractere para um nï¿½mero
             uint8_t digit = receivedChar - '0';
 
-            // Atualiza o campo apropriado do relógio
+            // Atualiza o campo apropriado do relï¿½gio
             switch (currentField)
             {
             case 0:  // Hour
-                //if (hour >=24){hour =0;}
+                if (hour >=10){hour =0;}
                 hour = hour * 10 + digit;
                 break;
             case 1:  // Minute
-                //if (minute >=60){minute =0;}
+                if (minute >=10){minute =0;}
                 minute = minute * 10 + digit;
                 break;
             case 2:  // Second
-                //if (second >=60){second =0;}
+                if (second >=10){second =0;}
                 second = second * 10 + digit;
                 break;
-            case 3:
-                hour = 0;
-                minute = 0;
-                second = 0;
-                currentField = 0;
             }
         }
     }
 }
 
+//************************* OPERAR RELOGIO *****************************
+
 void updateClock(uint8_t *hour, uint8_t *minute, uint8_t *second)
-//void updateClock(uint16_t *hour, uint16_t *minute, uint16_t *second)
 {
     // Incrementa o segundo
     (*second)++;
@@ -89,27 +103,152 @@ void updateClock(uint8_t *hour, uint8_t *minute, uint8_t *second)
     }
 }
 
-//void Timer0IntHandler(void)
-//{
-//    // Limpa a bandeira de interrupção do temporizador
-//    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-//
-//    updateClock(&hour, &minute, &second);
-//
-//    lcdInit();
-//    lcdSetCursor(3, 2);
-//    lcdWriteUnsignedInt16(hour);
-//    lcdWriteString(":");
-//    lcdWriteUnsignedInt16(minute);
-//    lcdWriteString(":");
-//    lcdWriteUnsignedInt16(second);
-//
-//    lcdClear();
-//}
+//************************* DIGITAL DISPLAY *****************************
+void DigitalDisplay (void){
+    updateClock(&hour, &minute, &second);
+
+    lcdSetCursor(2, 2);
+    lcdWriteUnsignedInt16(hour);
+    lcdWriteString(":");
+    lcdWriteUnsignedInt16(minute);
+    lcdWriteString(":");
+    lcdWriteUnsignedInt16(second);
+}
+
+//************************* ANALOG DISPLAY *****************************
+void draw_bars(int altura, int width, int starti, int startj){
+    for (int i =starti; i<altura; i++){
+        int xPos = i*width;
+        for (int j = 0; j<altura; j++){
+            lcdBufferSetPixel(xPos, j);
+        }
+    }
+    for(int j = startj; j<60; j++){
+        lcdBufferSetPixel(j, startj);
+        lcdBufferSetPixel(j, altura-1);
+    }
+
+}
+
+void draw_box (int start, int end){
+    for (int k = start; k < end; k++){
+        lcdBufferSetPixel(0, k);
+        lcdBufferSetPixel(60, k);
+    }
+
+    for(int g = 0; g<60; g++){
+        lcdBufferSetPixel(g, start);
+        lcdBufferSetPixel(g, end -1);
+    }
+}
+
+void draw_lines(int start, int end, int startX, int endX) {
+    for (int x = startX; x < endX; x++) {
+        for (int k = start; k < end; k++) {
+            lcdBufferSetPixel(x, k);
+        }
+    }
+}
+
+void erase_lines(int start, int end, int startX, int endX) {
+    for (int x = startX; x < endX; x++) {
+        for (int k = start; k < end; k++) {
+            lcdBufferClearPixel(x, k);
+        }
+    }
+}
+int pos_horas[NUM_HORAS][4];
+int pos_min[NUM_MIN][2];
+int pos_sec [NUM_SEC][2];
+void preencherPosHoras(int pos_horas[][4]) {
+    for (int i = 0; i < NUM_HORAS; i++) {
+        pos_horas[i][0] = 1;
+        pos_horas[i][1] = 12;
+        pos_horas[i][2] = i * 5 + 1;
+        pos_horas[i][3] = i * 5 + 5;
+        }
+}
+
+void preencherMS(void){
+    for (int i = 0; i < NUM_MIN; i++){
+        pos_min[i][0] = i;
+        pos_sec[i][0] = i;
+
+        pos_min[i][1] = i+1;
+        pos_sec[i][1] = i+1;
+    }
+
+}
+
+void AnalogDisplay (void) {
+    int nhour;
+    draw_bars(13, 5, 0, 0);
+    draw_box(17,30);
+    draw_box(34,48);
+    updateClock(&hour, &minute, &second);
+
+
+    if (hour > 12){
+        nhour = hour - 12;} //transforma 24h para 12h
+    else {
+        nhour = hour;}
+
+    draw_lines(1,12,pos_horas[nhour-1][2], pos_horas[nhour-1][3]);
+    draw_lines(18,29, pos_min[minute][0], pos_min[minute][1]);
+    draw_lines(35,47, pos_sec[second][0], pos_sec[second][1]);
+    lcdBufferDisplay();
+    if (minute == 59){
+        for (int i = 0; i<60; i++){
+            erase_lines(17, 30, pos_min[i][0],pos_min[i][1]);
+            erase_lines(1,12,pos_horas[nhour-1][2], pos_horas[nhour-1][3]);
+        }
+
+    }
+
+    if (second == 59){
+        for (int i = 0; i<60; i++){
+            erase_lines(34, 48, pos_sec[i][0],pos_sec[i][1]);
+        }
+
+    }
+
+}
+
+//************************* TIMER HANDLER *****************************
+int secondsCounter = 0;
+void Timer0IntHandler(void)
+{
+    // Limpa a bandeira de interrupï¿½ï¿½o do temporizador
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    if (secondsCounter % 10 == 0) {
+            flipflop = !flipflop;
+    }
+
+            // Alterna entre os modos analï¿½gico e digital a cada 10 segundos
+    if (flipflop) {
+        AnalogDisplay();
+    } else {
+        lcdBufferClear();
+        lcdBufferDisplay();
+        DigitalDisplay();
+    }
+
+        // Atualiza o contador de segundos
+        secondsCounter++;
+
+        // Certifique-se de reiniciar o contador de segundos apï¿½s 60 para evitar estouro
+        if (secondsCounter >= 20) {
+            secondsCounter = 0;
+        }
+}
+
+//************************* MAIN *****************************
 int main(void)
 {
     //Configs Gerais
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    // Configs UART
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     GPIOPinConfigure(GPIO_PA0_U0RX);
@@ -121,14 +260,15 @@ int main(void)
     IntEnable(INT_UART0); //enable the UART interrupt
     UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT); //only enable RX and TX interrupts
 
-//    // Configs Timer
-//    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-//    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC); // Configuração do Timer0 para contar até a frequência do clock do sistema
-//    TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet());
-//    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);// Registra a função de tratamento de interrupção
-//    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT); // Habilita a interrupção do temporizador quando o valor do temporizador atinge 0
-//    TimerEnable(TIMER0_BASE, TIMER_A);// Habilita o temporizador
-
+    // Configs Timer
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+    ui32Period = (SysCtlClockGet() / 1); // Periodo 1 segundo
+    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period -1);
+    IntEnable(INT_TIMER0A);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    IntMasterEnable();
+    TimerEnable(TIMER0_BASE, TIMER_A);
 
     UARTCharPut(UART0_BASE, 'E');
     UARTCharPut(UART0_BASE, 'n');
@@ -145,21 +285,14 @@ int main(void)
 
     //Configs Display
     lcdInit();
+    preencherPosHoras(pos_horas);
+    preencherMS();
 
 
     while(1)
     {
-        updateClock(&hour, &minute, &second);
-        lcdSetCursor(3, 2);
-
-        lcdWriteUnsignedInt16(hour);
-        lcdWriteString(":");
-        lcdWriteUnsignedInt16(minute);
-        lcdWriteString(":");
-        lcdWriteUnsignedInt16(second);
-
-        SysCtlDelay(SysCtlClockGet()/4);
-        lcdClear();
+//        DigitalDisplay();
+//        AnalogDisplay();
     }
 
 }
